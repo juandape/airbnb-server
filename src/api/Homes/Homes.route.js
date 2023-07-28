11e0dfccb53b7d026bbcb5fafaa0b23f3ef5c888
Juan David Peña <juandavidp76@hotmail.com>
2023-07-27 22:51:58 -0500

    fix homes route image post

--- src/api/Homes/Homes.route.js
+++ src/api/Homes/Homes.route.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const homesController = require("./Homes.controller");
 const { auth } = require('../Utils/auth');
-const { uploadImg } = require('../Upload/upload.service');
+const { uploadMultipleImg } = require('../Upload/upload.controller');
 // const formData = require("../Utils/formData");
 
 router.route("/").get(homesController.list)
 router.route('/listings').get(auth,homesController.showUser)
 router.route('/filter').post(homesController.listFilter)
 router.route("/:homeId").get(homesController.show)
-router.route("/").post(auth,uploadImg,homesController.create)
+router.route("/").post(auth,uploadMultipleImg,homesController.create)
 router.route("/:homeId").put(auth,homesController.update)
 router.route("/:homeId").delete(auth,homesController.destroy)
 
