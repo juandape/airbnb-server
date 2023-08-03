eed4c8a66c4995baea7109e4abcac84e4faf4b6e
Juan David Peña <juandavidp76@hotmail.com>
2023-08-02 22:10:41 -0500

    feat: swagger

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require('./app');
+const { swaggerDocs: V1SwaggerDocs } = require('./src/v1/swagger');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}!`)
-);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}!`);
+  V1SwaggerDocs(app, port);
+});
 
 module.exports = app;
--- routes.js
+++ routes.js
@@ -7,16 +7,148 @@ const formData = require('./src/api/Utils/formData');
 const paymentsRoute = require('./src/api/Payments/Payment.route');
 
 function router(app) {
-  app.use('/homes', homesRoute);
-  app.use('/user', userRoute);
-  app.use('/reservations', reservationRoute);
-  app.use('/comments', commentsRoute);
+  /**
+ * @openapi
+ * /api/homes:
+ *   get:
+ *     tags:
+ *       - Homes
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
+  app.use('/api/homes', homesRoute);
+  /**
+ * @openapi
+ * /api/user:
+ *   get:
+ *     tags:
+ *       - Users
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
+  app.use('/api/user', userRoute);
+  /**
+ * @openapi
+ * /api/reservations:
+ *   get:
+ *     tags:
+ *       - Reservations
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
+  app.use('/api/reservations', reservationRoute);
+  /**
+ * @openapi
+ * /api/comments:
+ *   get:
+ *     tags:
+ *       - Comments
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
+  app.use('/api/comments', commentsRoute);
+  /**
+ * @openapi
+ * /api/upload:
+ *   get:
+ *     tags:
+ *       - Uploads
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
   app.use('/api/upload', uploadRouter);
-  app.use('/payments', paymentsRoute);
+  /**
+ * @openapi
+ * /api/payments:
+ *   get:
+ *     tags:
+ *       - Payments
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
+  app.use('/api/payments', paymentsRoute);
 
-  app.post('/prueba', formData, (req, res) => {
-    res.status(200).json({ ...req.body });
-  });
+  // app.post('/prueba', formData, (req, res) => {
+  //   res.status(200).json({ ...req.body });
+  // });
 }
 
 module.exports = router;
